79201b7c7f9bbecd3059dbbaa8db530ffd20a613
parthbatt <143800471+parthbatt@users.noreply.github.com>
2023-09-21 15:14:26 +0530

    Add files via upload

---
+++ streamlit_app.py
@@ -0,0 +1,12 @@
+import streamlit as st
+
+
+st.set_page_config(page_title="parth''s website", page_icon=":tada:", layout="wide")
+st.subheader("hi i am parth")
+st.write("---")
+st.header("what do i do")
+st.write("i'am an python devloper and i  will make a python tutorial to make this website and deploy it to streamlit.com/cloud ")
+st.header('why i made this') 
+st.write("for fun")
+st.write("---")
+st.write("[the code>](https://1stwebpage.streamlit.app/)")
